06a81d6af519713fb0d48592c33b174fa4f63087
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-01-20 15:45:12 -0600

    set up the api use effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BASE_URL, API_KEY } from "./constants";
-
+import axios from "axios"
+import Nav from './components/Nav'
+import Card from './components/Card'
 
 function App() {
+//use states here
+const [nasaData, setNasaData] = useState({});
+const [date, setDate] = useState('2020-01-20');
+
+//call use effect to get the nasa data
+
+useEffect(() => {
+  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+  .then(response => {console.log(response.data)})
+  .catch(error => {(console.log(error))})
+},[]);
+
   return (
     <div className="App">
       <p>
